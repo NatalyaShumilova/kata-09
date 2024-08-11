5f6cfb88b24433f49e8584f10ff5e984ffba0a37
NatalyaShumilova <natalya.shumilova@gmail.com>
2024-08-11 14:26:36 +1200

    returns correct sum given unique items

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -1,16 +1,30 @@
 const CheckOut = require("../index");
 
-const rules = [
-    {item: "A", unitPrice: 50, specialPrice: {units: 3, price: 130}},
-    {item: "B", unitPrice: 30, specialPrice: {units: 2, price: 45}},
-    {item: "C", unitPrice: 20},
-    {item: "D", unitPrice: 15}
-];
+const rules = {
+   A: {unitPrice: 50, specialPrice: {units: 3, price: 130}},
+   B: {unitPrice: 30, specialPrice: {units: 2, price: 45}},
+   C: {unitPrice: 20},
+   D: {unitPrice: 15}
+};
 
 describe("CheckOut", () => {
     it("returns 0 given empty shopping cart", () => {
         const co = new CheckOut(rules);
         co.scan("");
         expect(co.total).toBe(0);
+    })
+
+    it("returns correct sum given unique items", () => {
+        const co1 = new CheckOut(rules);
+        co1.scan("A");
+        expect(co1.total).toBe(50);
+
+        const co2 = new CheckOut(rules);
+        co2.scan("AB");
+        expect(co2.total).toBe(80);
+
+        const co3 = new CheckOut(rules);
+        co3.scan("CDBA");
+        expect(co3.total).toBe(115);
     })
 })
--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 class CheckOut {
     constructor(rules) {
         this.total = 0;
+        this.rules = rules;
     }
 
     scan(items) {
-
+        [...items].forEach(i => {
+            this.total += this.rules[i].unitPrice
+        })
     }
      
 } 
